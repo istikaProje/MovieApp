8747c27f1db91f69a708cb16dd25f3df4fb88fee
Egemen Öztunçel <egemen.oztuncel@hotmail.com>
2025-01-19 18:46:49 +0300

    bug oldu commit etmem lazım

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -113,23 +113,4 @@
  </div>
 </div>
 
-<script>
-    document.addEventListener('DOMContentLoaded', (event) => {
-        const checkbox = document.getElementById('check-with-link');
-        const submitButton = document.getElementById('submit-button');
-        const form = document.getElementById('register-form');
-
-        // Form gönderimi sırasında kontrol
-        form.addEventListener('submit', (e) => {
-            if (!checkbox.checked) {
-                e.preventDefault(); // Checkbox işaretlenmemişse gönderimi durdur
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Lütfen hizmet şartlarını kabul edin.',
-                });
-            }
-        });
-    });
-</script>
 @endsection
