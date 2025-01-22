bc591bae4de46314efa5e9f959789aaf4a3449ae
beyzaaaanur <149897085+beyzaaaanur@users.noreply.github.com>
2025-01-22 11:55:54 +0300

    seçilen kategorilerden filmleri seçerek kullanıcıya özel listem dosyasında kayıt ederek listeler

--- resources/views/layouts/sections/home/_slideSecond.blade.php
+++ resources/views/layouts/sections/home/_slideSecond.blade.php
@@ -1,46 +1,172 @@
-
 <div class="bg-third p-5 rounded-md mb-5 mt-5">
-        
-       
     <div class="swiper slider2">
-
         <div class="pb-1">
-
-        <h2 class="bold mb-2 text-xl text-white">Film koleksiyonları</h2>
-        <div class="swiper-button-prev custom-prev"></div>
-        <div class="swiper-button-next custom-next"></div>
-
+            <h2 class="bold mb-2 text-xl text-white">Film/Dizi</h2>
+            <div class="swiper-button-prev custom-prev"></div>
+            <div class="swiper-button-next custom-next"></div>
         </div>
-        
-        <div class="swiper-wrapper">
-            <!-- Card -->
-            <div class="swiper-slide group relative rounded-lg overflow-hidden">
-              <img src="https://picsum.photos/300/200" alt="Sample" class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105">
-              <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-70"></div>
-              <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-                <button class="px-4 py-2 bg-white text-black rounded-lg">Details</button>
-              </div>
-            </div>
-            <div class="swiper-slide group relative rounded-lg overflow-hidden">
-              <img src="https://picsum.photos/300/200" alt="Sample" class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105">
-              <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-70"></div>
-              <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-                <button class="px-4 py-2 bg-white text-black rounded-lg">Details</button>
-              </div>
-            </div>
-            <div class="swiper-slide group relative rounded-lg overflow-hidden">
-              <img src="https://picsum.photos/300/200" alt="Sample" class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105">
-              <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-70"></div>
-              <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-                <button class="px-4 py-2 bg-white text-black rounded-lg">Details</button>
-              </div>
-            </div>
-          </div>
-  
 
-     
-  
+        <!-- Grid Container -->
+        <div class="grid grid-cols-4 gap-4">
+            <!-- Üst Satır -->
 
+            <div class="swiper-slide group relative rounded-lg overflow-hidden" data-id="1">
+                <img src="{{asset('images/breaking-bad.jpg')}}" alt="Sample" class="w-full h-auto aspect-[16/9] object-cover transition-transform duration-300 group-hover:scale-105">
+                <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-10"></div>
+                <div class="absolute inset-0 flex items-center justify-center opacity-100 transition-opacity">
+                    <div class="absolute top-2 right-2 opacity-100">
+                        <button type="button" class="add-to-favorites" data-movie-id="1" data-image-url="{{ asset('images/breaking-bad.jpg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-6 w-6 text-white hover:text-red-500 transition duration-300">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>   
+
+            <div class="swiper-slide group relative rounded-lg overflow-hidden" data-id="2">
+                <img src="{{asset('images/la-casa-de-papel.jpg')}}" alt="Sample" class="w-full h-auto aspect-[16/9] object-cover transition-transform duration-300 group-hover:scale-105">
+                <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-10"></div>
+                <div class="absolute inset-0 flex items-center justify-center opacity-100 transition-opacity">
+                    <div class="absolute top-2 right-2 opacity-100">
+                        <button type="button" class="add-to-favorites" data-movie-id="2" data-image-url="{{ asset('images/la-casa-de-papel.jpg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-6 w-6 text-white hover:text-red-500 transition duration-300">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="swiper-slide group relative rounded-lg overflow-hidden" data-id="3">
+                <img src="{{asset('images/lucy.jpg')}}" alt="Sample" class="w-full h-auto aspect-[16/9] object-cover transition-transform duration-300 group-hover:scale-105">
+                <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-10"></div>
+                <div class="absolute inset-0 flex items-center justify-center opacity-100 transition-opacity">
+                    <div class="absolute top-2 right-2 opacity-100">
+                        <button type="button" class="add-to-favorites" data-movie-id="3" data-image-url="{{ asset('images/lucy.jpg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-6 w-6 text-white hover:text-red-500 transition duration-300">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+                            </svg>
+                        </button>
+                    </div>              
+                </div>
+            </div>
+
+            <div class="swiper-slide group relative rounded-lg overflow-hidden" data-id="4">
+                <img src="{{asset('images/maymunlar-c.jpg')}}" alt="Sample" class="w-full h-auto aspect-[16/9] object-cover transition-transform duration-300 group-hover:scale-105">
+                <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-10"></div>
+                <div class="absolute inset-0 flex items-center justify-center opacity-100 transition-opacity">
+                    <div class="absolute top-2 right-2 opacity-100">
+                        <button type="button" class="add-to-favorites" data-movie-id="4" data-image-url="{{ asset('images/maymunlar-c.jpg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-6 w-6 text-white hover:text-red-500 transition duration-300">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Alt Satır -->
+            <div class="swiper-slide group relative rounded-lg overflow-hidden" data-id="5">
+                <img src="{{asset('images/the-terminal-list.jpg')}}" alt="Sample" class="w-full h-auto aspect-[16/9] object-cover transition-transform duration-300 group-hover:scale-105">
+                <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-10"></div>
+                <div class="absolute inset-0 flex items-center justify-center opacity-100 transition-opacity">
+                    <div class="absolute top-2 right-2 opacity-100">
+                        <button type="button" class="add-to-favorites" data-movie-id="5" data-image-url="{{ asset('images/the-terminal-list.jpg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-6 w-6 text-white hover:text-red-500 transition duration-300">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+                
+
+            <div class="swiper-slide group relative rounded-lg overflow-hidden" data-id="6">
+                <img src="{{asset('images/stranger-things.jpg')}}" alt="Sample" class="w-full h-auto aspect-[16/9] object-cover transition-transform duration-300 group-hover:scale-105">
+                <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-10"></div>
+                <div class="absolute inset-0 flex items-center justify-center opacity-100 transition-opacity">
+                    <div class="absolute top-2 right-2 opacity-100">
+                        <button type="button" class="add-to-favorites" data-movie-id="6" data-image-url="{{ asset('images/stranger-things.jpg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-6 w-6 text-white hover:text-red-500 transition duration-300">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+                            </svg>
+                        </button>
+                    </div>             
+                </div>
+            </div>
+
+
+            <div class="swiper-slide group relative rounded-lg overflow-hidden" data-id="7">
+                <img src="{{asset('images/prison-break.jpg')}}" alt="Sample" class="w-full h-auto aspect-[16/9] object-cover transition-transform duration-300 group-hover:scale-105">
+                <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-10"></div>
+                <div class="absolute inset-0 flex items-center justify-center opacity-100 transition-opacity">
+                    <div class="absolute top-2 right-2 opacity-100">
+                        <button type="button" class="add-to-favorites" data-movie-id="7" data-image-url="{{ asset('images/prison-break.jpg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-6 w-6 text-white hover:text-red-500 transition duration-300">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+                            </svg>
+                        </button>
+                    </div>             
+                </div>
+            </div> 
+
+
+            <div class="swiper-slide group relative rounded-lg overflow-hidden" data-id="8">
+                <img src="{{asset('images/the-walking-dead.jpg')}}" alt="Sample" class="w-full h-auto aspect-[16/9] object-cover transition-transform duration-300 group-hover:scale-105">
+                <div class="absolute inset-0 bg-black bg-opacity-40 transition-opacity group-hover:bg-opacity-10"></div>
+                <div class="absolute inset-0 flex items-center justify-center opacity-100 transition-opacity">
+                    <div class="absolute top-2 right-2 opacity-100">
+                        <button type="button" class="add-to-favorites" data-movie-id="8" data-image-url="{{ asset('images/the-walking-dead.jpg') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-6 w-6 text-white hover:text-red-500 transition duration-300">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+                            </svg>
+                        </button>
+                    </div>              
+                </div>
+            </div> 
+        </div>
     </div>
-
 </div>
+
+<script>
+    document.querySelectorAll('.add-to-favorites').forEach(button => {
+        button.addEventListener('click', function() {
+            const movieId = this.getAttribute('data-movie-id');
+            const imageUrl = this.getAttribute('data-image-url');
+
+            fetch('{{ route('favorites.store') }}', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+                body: JSON.stringify({ movie_id: movieId, image_url: imageUrl })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    alert('Film favorilerinize eklendi!');
+                } else {
+                    alert(data.message);
+                }
+            });
+        });
+    });
+</script>
+
+
+
+
+
+
+
+
