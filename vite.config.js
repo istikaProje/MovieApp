5bacd112dc6b84af6877e9806f8bbcde8788c509
yasinkarali <yasinkarali8@gmail.com>
2025-01-24 14:13:26 +0300

    Yorum satırına profil resmi dinamik şekilde getirildi ve vite ile ilgili sorunlar düzeltildi.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js' , 'resources/css/swiper.css',
-                'resources/js/swiper.js', 'resources/js/toggleFavorite.js', 'resources/js/sliders.js','resources/js/stripe.js','resources/js/register.js' ], 
+                'resources/js/swiper.js', 'resources/js/toggleFavorite.js', 'resources/js/sliders.js','resources/js/stripe.js','resources/js/register.js','video.js/dist/video-js.css' ],
             refresh: true,
         }),
     ],
